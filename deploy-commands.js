14647563d6cae7c38328ea274bbfd6377b225bfc
MasutaYunikon <quentin.robert@epitech.eu>
2023-11-19 19:36:32 +0100

    refactor: implement __dirname equivalent in ES module

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,12 +1,11 @@
 import { REST, Routes } from 'discord.js';
 import fs from 'fs';
 import path from 'path';
+import {fileURLToPath} from "node:url";
 
 const clientId = process.env.CLIENT_ID;
 const token = process.env.DISCORD_TOKEN;
-
-console.log("Token:", process.env.DISCORD_TOKEN);
-console.log("Client ID:", process.env.CLIENT_ID);
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const commands = [];
 // Grab all the command files from the commands directory you created earlier
@@ -36,7 +35,6 @@ const rest = new REST().setToken(token);
 (async () => {
     try {
         console.log(`Started refreshing ${commands.length} application (/) commands.`);
-
         // The put method is used to fully refresh all commands in the guild with the current set
         const data = await rest.put(
             Routes.applicationCommands(clientId),
